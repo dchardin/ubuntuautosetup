746489adfe872d75ac247251c5d73cb810fdac74
dchardin <dch84121@gmail.com>
2017-06-17 22:11:06 -0400

    deleting some package entries

--- ubuntuautosetup.sh
+++ ubuntuautosetup.sh
@@ -46,89 +46,39 @@ apt_get_install_apps()
 		"automake" \
 		"binutils" \
 		"byzanz" \
-		"bzip2-devel" \
 		"cmake" \
 		"curl" \
-		"curl-devel" \
 		"devilspie" \
 		"dkms" \
 		"dos2unix" \
 		"evince" \
-		"expat-devel" \
 		"expect" \
-		"gcc" \
-		"gcc-c++" \
 		"git" \
-		"glibc-devel" \
-		"glibc-headers" \
 		"gnupg" \
 		"golang" \
-		"google-chrome-stable" \
-		"gstreamer" \
-		"gstreamer-ffmpeg" \
-		"gstreamer-plugins-bad" \
-		"gstreamer-plugins-bad-free" \
-		"gstreamer-plugins-bad-free-extras" \
-		"gstreamer-plugins-bad-nonfree" \
-		"gstreamer-plugins-base" \
-		"gstreamer-plugins-good" \
-		"gstreamer-plugins-ugly" \
 		"htop" \
 		"hexchat" \
-		"insync" \
-		"k3b-extras-freeworld" \
-		"kernel-devel" \
-		"kernel-headers" \
-		"libdvbpsi" \
-		"libdvdcss" \
-		"libdvdnav" \
-		"libdvdread" \
-		"libgomp" \
 		"lsdvd" \
 		"m4" \
 		"make" \
 		"mercurial" \
-		"ncurses-devel" \
 		"nmap" \
-		"openssh" \
 		"openvpn" \
 		"patch" \
 		"python-argparse" \
-		"python-devel" \
-		"python-urllib2_kerberos" \
-		"pytz" \
 		"ruby" \
 		"rxvt" \
 		"screen" \
 		"screenfetch" \
 		"sound-juicer" \
-		"steam" \
 		"terminator" \
 		"texinfo" \
 		"tmux" \
 		"unzip" \
 		"vim" \
 		"vlc" \
-		"xfreerdp" \
-		"xine-lib" \
-		"xine-lib-extras" \
-		"xine-lib-extras-freeworld" \
-		"zlib-devel" \
 		"unzip" \
-		"dvdstyler" \
 		"devede" \
-		"simplescreenrecorder" \
-		"qt5-qtbase" \
-		"qt5-qtbase-devel" \
-		"qt5-qtdeclarative" \
-		"qt5-qtdeclarative-devel" \
-		"qt5-qtgraphicaleffects" \
-		"qt5-qtquickcontrols" \
-		"redhat-rpm-config" \
-		"flash-plugin" \
-		"nspluginwrapper" \
-		"alsa-plugins-pulseaudio" \
-		"libcurl" \
 		"gpick" \
 	)
 
